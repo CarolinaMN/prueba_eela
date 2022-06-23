874cb654e444296dedbca36006a61c097ce654c1
Dolores Molina <mcaro2796@gmail.com>
2022-06-22 19:53:11 -0500

    Inicio de proyecto

---
+++ funciones.py
@@ -0,0 +1,38 @@
+import math
+
+def primo(num):
+    if num < 0:
+        return 'No se permiten números negativos.'
+
+    if num <= 1:
+        return False
+
+    if num == 2:
+        return True
+
+    if num % 2 == 0:
+        return False
+
+    for n in range(2, num):
+        if num % n == 0:
+            print("No es primo", n, "es divisor")
+            return False
+    print("Es primo")
+    return True
+
+
+
+def cubica(a):
+    return a * a * a
+
+
+def saludo(name):
+    return "Hola, " + name
+
+
+primo(21)
+
+primo(0)
+primo(20)
+primo(5888)
+primo(78)
---
+++ test_funciones.py
@@ -0,0 +1,30 @@
+
+import unittest
+import funciones
+
+class TestUtils(unittest.TestCase):
+    def test_is_prime(self):
+        self.assertFalse(funciones.primo(4))
+        self.assertTrue(funciones.primo(2))
+        self.assertTrue(funciones.primo(3))
+        self.assertFalse(funciones.primo(8))
+        self.assertFalse(funciones.primo(10))
+        self.assertTrue(funciones.primo(7))
+        self.assertEqual(funciones.primo(-3), "No se permiten números negativos.")
+    
+    def test_cubic(self):
+        self.assertEqual(funciones.cubica(2), 8)
+        self.assertEqual(funciones.cubica(-2), -8)
+        self.assertNotEqual(funciones.cubica(2), 4)
+        self.assertNotEqual(funciones.cubica(-3), 27)
+
+    def test_say_hello(self):
+        self.assertEqual(funciones.saludo("Juan"), "Hola, Juan")
+        self.assertEqual(funciones.saludo("Carlos"), "Hola, Carlos")
+        self.assertNotEqual(funciones.saludo("Lili"), "Hola, Lilii")
+        self.assertNotEqual(funciones.saludo("Maria"), "Hola, Mariaa")
+
+
+if __name__ == '__main__':
+    unittest.main()
+
